212b5d350b55c21afb7d2894fc246e6af8849e4d
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2019-01-28 05:48:05 +0700

    - Add new edit-profile routes

--- modules/user/user.router.php
+++ modules/user/user.router.php
@@ -239,6 +239,73 @@ use \DavidePastore\Slim\Validation\Validation;
         ->add($container->get('csrf'))
         ->add(new SessionCheck($container->get('router')));
 
+    // GET Edit Profile page
+    $app->get('/edit-profile/{username}', function (Request $request, Response $response) {
+        $sh = new SessionHelper();
+        $user = new UserManager();
+        $user->username = $request->getAttribute('username'); 
+        $data = $user->read();
+        $data['option_status'] = $user->optionStatus();
+        $data['username'] = $sh->get('username');
+        $data['avatar'] = $sh->get('avatar');
+        // Remove the data status from reading data
+        if($data['status'] == 'success') {
+            unset($data['status']);
+            unset($data['message']);
+        }
+        // Get if there is any flash message before request
+        if($this->flash->hasMessage('update')){
+            $message = $this->flash->getMessage('update');
+            // create new data status for response in twig
+            $data['status'] = $message[0]['status'];
+            $data['message'] = $message[0]['message'];
+            $data['problem'] = $message[0]['problem'];
+        }
+        return $this->view->render($response, "profile-edit.twig", $data);
+    })->setName("/edit-profile")
+        ->add($container->get('csrf'))
+        ->add(new SessionCheck($container->get('router')));
+
+    // POST Edit Profile page
+    $app->post('/edit-profile/{username}', function (Request $request, Response $response) {
+        $datapost = $request->getParsedBody();
+        if($request->getAttribute('has_errors')){
+            $errors = $request->getAttribute('errors');
+            $data = [
+                'status' => 'error',
+                'message' => 'Parameter is not valid! ',
+                'problem' => json_encode($errors)
+            ];
+        } else {
+            $user = new UserManager();
+            $user->username = $datapost['username'];
+            $user->email = $datapost['email'];
+            $user->firstname = $datapost['firstname'];
+            $user->lastname = $datapost['lastname'];
+            $user->address = $datapost['address'];
+            $user->city = $datapost['city'];
+            $user->country = $datapost['country'];
+            $user->postal = $datapost['postal'];
+            $user->avatar = $datapost['avatar'];
+            $user->background_image = $datapost['background_image'];
+            $user->about = $datapost['about'];
+            $user->status = $datapost['status'];
+            $sh = new SessionHelper();
+            $user->updated_by = $sh->get('username');
+            $data = $user->updateAsAdmin();
+            $data['problem'] = '';
+        }
+
+        // Create flash message to next redirected url
+        $this->flash->addMessage('update',['status' => $data['status'],'message' => $data['message'],'problem' => $data['problem']]);
+        // Redirect to same page
+        $url = $request->getUri()->withPath($this->router->pathFor('/edit-profile',['username' => $datapost['username']]));
+        return $response->withRedirect($url);
+    })->setName("/edit-profile")
+        ->add(new Validation(validator::update()))
+        ->add($container->get('csrf'))
+        ->add(new SessionCheck($container->get('router')));
+
     // GET Change Password page
     $app->get('/change-password', function (Request $request, Response $response) {
         $sh = new SessionHelper();
